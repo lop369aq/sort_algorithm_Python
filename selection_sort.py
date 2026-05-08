412461df8cf6ac37861c25ceaee736ef34d2096e
Criptx <criptx@users.noreply.github.com>
2026-05-09 00:15:22 +0900

    Add selection sort implementation

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+        change = False
+        for j in range(i+1, n):
+            
+            if arr[min_index] > arr[j]:
+                min_index = j
+                change = True
+            
+        if change:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr  
+
+data = [29, 10, 14, 37, 13]
+print("Input data :", data)
+print("Output data :", selection_sort(data))
